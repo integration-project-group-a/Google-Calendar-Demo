fb218cbe01cf0f30089083b44ea05c27da55344a
iggy <igor.r_@outlook.com>
2019-03-18 11:16:12 +0100

    class Calendarss fully functional + extra functionality + extra methods

--- CalendarQuickstart/Logic/Calendarss.cs
+++ CalendarQuickstart/Logic/Calendarss.cs
@@ -17,102 +17,100 @@ namespace CalendarQuickstart.Logic
             service = GService.service;
         }
 
-        public static Calendar getCalender(Calendar calenderToGet)
+        public static IList<CalendarListEntry> getCalenders()
         {
+            var calenders = service.CalendarList.List().Execute().Items;
 
+            return calenders;
+        }
+
+        public static Calendar getCalender(Calendar calenderToGet)
+        {        
             Calendar calander = service.Calendars.Get(calenderToGet.Id).Execute();
 
             return calander;
         }
-        public static Calendar getCalenderById(String id)
+        //primary is the main calendar of this project
+        public static Calendar getCalenderById(String id = "primary")
         {
-
             Calendar calander = service.Calendars.Get(id).Execute();
 
             return calander;
         }
+        //this is not with type Calander, make sure to use var or CalendarListEntry when using
         public static CalendarListEntry getCalenderByName(String name)
-        {
-            //this is not with type Calander, make sure to use var or CalendarListEntry when using
-
+        {     
             var calenders = getCalenders();
 
             foreach (CalendarListEntry cal in calenders)
             {
-
                 if (cal.Summary == name)
                 {
                     return cal;
                 }
             }
 
+            //calendar doesn't excist
             return null;
         }
 
-        public static IList<CalendarListEntry> getCalenders()
-        {
+        //you can only clear the primary calendar
+        //BE AWARE this is a very desctructive method, all events will be deleted
+        //test string
+        public static string clearCalendar() {
 
-            var calenders = service.CalendarList.List().Execute().Items;
-
-            return calenders;
-
+            return service.Calendars.Clear("primary").Execute();
         }
 
-        public static CalendarsResource.InsertRequest newCalendar(string name, string location, string timeZone = "America/Los_Angeles")
+        //creates secondary calendar
+        //probably not used...
+        public static Calendar newCalendar(string name, string location, string despcription, string secundaryCalendarId, string timeZone = "America/Los_Angeles")
         {
-
             Calendar newCalendar = new Calendar
             {
                 Summary = name,
                 Location = location,
-                TimeZone = timeZone
-
-
+                TimeZone = timeZone,
+                Description = despcription,
+                Id = secundaryCalendarId
             };
 
-            //probably not used...
-            return service.Calendars.Insert(newCalendar);
-
+            return service.Calendars.Insert(newCalendar).Execute();
         }
 
-        public static void updateCalendarById(string id, string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles")
+        //RECHEARCH NEEDED: change id possible?
+        public static Calendar updateCalendarById(string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles", string id = "primary")
         {
-
             Calendar calendar = service.Calendars.Get(id).Execute();
 
-
             // Make a change
-            calendar.Summary = "new Summary";
-            calendar.Location = newName;
+            calendar.Summary = newName;
             calendar.Location = newLocation;
+            calendar.TimeZone = newTimeZone;
             calendar.Description = newDescription;
 
             // Update the altered calendar
-            service.Calendars.Update(calendar, calendar.Id).Execute();
-
+            return service.Calendars.Update(calendar, calendar.Id).Execute();
         }
-        public static void updateCalendar(Calendar calendar, string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles")
+        public static Calendar updateCalendar(Calendar calendar, string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles")
         {
             // Make a change
-            calendar.Summary = "new Summary";
-            calendar.Location = newName;
+            calendar.Summary = newName;
             calendar.Location = newLocation;
+            calendar.TimeZone = newTimeZone;
             calendar.Description = newDescription;
 
             // Update the altered calendar
-            service.Calendars.Update(calendar, calendar.Id).Execute();
-
+            return service.Calendars.Update(calendar, calendar.Id).Execute();
         }
-        //not tested!!!
-        public static void updateCalendarByName(string oldName, string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles")
+        //not yet tested!!
+        public static Calendar updateCalendarByName(string oldName, string newName, string newLocation = "unknown", string newDescription = "none", string newTimeZone = "America/Los_Angeles")
         {
-
             var calenders = getCalenders();
             CalendarListEntry foundCalendar = null;
 
             foreach (CalendarListEntry cal in calenders)
             {
-
                 if (cal.Summary == oldName)
                 {
                     foundCalendar = cal;
@@ -120,46 +118,42 @@ namespace CalendarQuickstart.Logic
             }
 
             // Make a change
-            Calendar calender = null;
-            calender.Summary = newName;
-            calender.Location = newLocation;
-            calender.Description = newDescription;
-            calender.TimeZone = newTimeZone;
-            calender.Id = foundCalendar.Id;
+            //because foundCalendar is a CalendarListEntry we have to convert it to a Calendar
+            Calendar calendar = null;
+            calendar.Summary = newName;
+            calendar.Location = newLocation;
+            calendar.Description = newDescription;
+            calendar.TimeZone = newTimeZone;
+            calendar.Id = foundCalendar.Id;
 
             // Update the altered calendar
-            service.Calendars.Update(calender, foundCalendar.Id).Execute();
+            return service.Calendars.Update(calendar, foundCalendar.Id).Execute();
 
         }
 
-        public static void deleteCalendar(Calendar calendar)
+        //whole block can only delete a secondary calendar
+        //test string return!!
+        public static string deleteCalendar(Calendar calendar)
         {
-
-            service.Calendars.Delete(calendar.Id).Execute();
-
+            return service.Calendars.Delete(calendar.Id).Execute();
         }
-        public static void deleteCalendarById(string id)
+        public static string deleteCalendarById(string id)
         {
-
-            service.Calendars.Delete(id).Execute();
-
+            return service.Calendars.Delete(id).Execute();
         }
-        public static void deleteCalendarByName(string name)
+        public static string deleteCalendarByName(string name)
         {
-
             var calenders = getCalenders();
             CalendarListEntry foundCalander = null;
 
             foreach (CalendarListEntry cal in calenders)
             {
-
                 if (cal.Summary == name)
                 {
                     foundCalander = cal;
                 }
             }
-            service.Calendars.Delete(foundCalander.Id).Execute();
-
+            return service.Calendars.Delete(foundCalander.Id).Execute();
         }
     }
 }
