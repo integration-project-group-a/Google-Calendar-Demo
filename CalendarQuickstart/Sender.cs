5f96784b756044fecf892bdf60b671e9952843bb
Jody Lambermont <jody.lambermont@student.ehb.be>
2019-04-04 15:06:12 +0200

    fix

---
+++ CalendarQuickstart/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Xml;
+using CalendarQuickstart.Logic;
+
+namespace CalendarQuickstart
+{
+    class Sender
+    {
+        public void SendMessage(string doc)
+        {
+            var factory = new ConnectionFactory() { HostName = "10.3.56.27", UserName = "manager", Password = "ehb", Port = 5672 };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var body = Encoding.UTF8.GetBytes(doc);
+                Console.WriteLine(body);
+                channel.BasicPublish(exchange: "RabbitMQ",
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+            }
+        }
+    }
+}
